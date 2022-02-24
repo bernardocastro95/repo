237ff3c1153921cf763149fb31d71b522e78b73e
Bernardo Castro <bernardoc544@gmail.com>
2022-02-24 15:42:40 -0500

    Main Style Defined

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
-
-import {Title} from './styles'
+import {FaGithub, FaPlus} from 'react-icons/fa'
+import {Container, Form, SubmitButton} from './styles'
 
 export default function Main(){
     return(
-        <Title>
-            Main
-        </Title>
+        <Container>
+            <FaGithub size={25}/>
+            <h1>My Repos</h1>
+
+            <Form onSubmit={()=>{}}>
+                <input type="text" placeholder='Add repos'/>
+
+                <SubmitButton>
+                    <FaPlus color="#FFF" size={14}/>
+                </SubmitButton>
+            </Form>
+        </Container>
     )
 }

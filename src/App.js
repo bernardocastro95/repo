0040408ccc0793cbbfeca7130513fba0f24c56d3
Bernardo Castro <bernardoc544@gmail.com>
2022-02-24 15:23:00 -0500

    Styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import RouteManager from './routes'
+import GlobalStyle from './styles/global'
 function App() {
   return (
+    <>
+      <GlobalStyle/>
+      <RouteManager/>
+    </>
     
-    <RouteManager/>
   );
 }
 

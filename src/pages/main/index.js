d11e391168ec8fff8997303d1beca1880b7b98c4
Bernardo Castro <bernardoc544@gmail.com>
2022-02-28 11:55:52 -0500

    Search Repo

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, {useState, useCallback} from 'react'
 import {FaGithub, FaPlus} from 'react-icons/fa'
 import {Container, Form, SubmitButton} from './styles'
+import api from '../../services/api'
 
 export default function Main(){
+
+    const[newRepo, setNewRepo] = useState('')
+    const[repos, setNewRepos] = useState([])
+
+    const handleSubmit = useCallback((e)=>{
+
+        e.preventDefault()
+
+        async function submit(){
+            const res = await api.get(`repos/${newRepo}`)
+            const data = {
+                name: res.data.full_name,
+            }
+            setNewRepos([...repos, data])
+            setNewRepo('')
+        } 
+        submit()   
+    }, [newRepo, repos])
+
+    function handleInputChange(e){
+        setNewRepo(e.target.value)
+    }
     return(
         <Container>
             <FaGithub size={25}/>
             <h1>My Repos</h1>
 
-            <Form onSubmit={()=>{}}>
-                <input type="text" placeholder='Add repos'/>
+            <Form onSubmit={handleSubmit}>
+                <input type="text" 
+                placeholder='Add repos'
+                value={newRepo}
+                onChange={handleInputChange}/>
 
                 <SubmitButton>
                     <FaPlus color="#FFF" size={14}/>
@@ -18,3 +44,5 @@ export default function Main(){
         </Container>
     )
 }
+
+
